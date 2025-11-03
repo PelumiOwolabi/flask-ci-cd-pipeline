39bc83fbcc2aff915f71d247a2cbe6064dc3ca07
Pelumi Owolabi <sammyswiss10@gmail.com>
2025-11-03 13:46:09 +0000

    Updated Flask app response and fixed test

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
+import werkzeug
+if not hasattr(werkzeug, '_version_'):
+    werkzeug._version_ = '3.0.0'
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hi there, this is a demonstration from my CI/CD pipeline."
+    return "Hi there, this is a demonstration from my CI/CD pipeline by Pelumi."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
